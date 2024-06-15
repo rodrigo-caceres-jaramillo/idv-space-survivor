61b8f1cf89deeb9ea3efab1a84b4aae6ecae2f4c
Rodrigo <rodicom10@gmail.com>
2024-06-14 23:33:05 -0300

    Vignette

---
+++ src/ui/hud/vignette.gd
@@ -0,0 +1,16 @@
+extends ColorRect
+
+var max_health
+func _ready():
+	self.max_health = Global.player.stats.MAX_HEALTH
+	self.check_low_health(Global.player.stats.HEALTH)
+	Global.player.stats.max_health_changed.connect(func (value): self.max_health = value)
+	Global.player.stats.health_changed.connect(self.check_low_health)
+
+func check_low_health(value):
+	var twentyPercentOfMax = max_health * 0.3
+	if(value < twentyPercentOfMax):
+		show()
+	if(value > twentyPercentOfMax):
+		hide()
+	
